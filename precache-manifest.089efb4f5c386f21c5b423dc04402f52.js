f5c0319deba826c8fe0c1dfc1ee74969c26d3a04
f.majidi <f.majidi@homing.ir>
2020-10-28 12:33:00 +0330

    Updates

---
+++ precache-manifest.089efb4f5c386f21c5b423dc04402f52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82815e19dbbf631b1f7855358a4cf824",
+    "url": "/search-list/index.html"
+  },
+  {
+    "revision": "b6f6065c82e0ba997964",
+    "url": "/search-list/static/css/main.eac756e0.chunk.css"
+  },
+  {
+    "revision": "9510ae799ffb48cdc69d",
+    "url": "/search-list/static/js/2.4346d48b.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/search-list/static/js/2.4346d48b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6f6065c82e0ba997964",
+    "url": "/search-list/static/js/main.54401141.chunk.js"
+  },
+  {
+    "revision": "db6da6bf64d647d67db9",
+    "url": "/search-list/static/js/runtime-main.64bd4139.js"
+  },
+  {
+    "revision": "231de5f3e0cba231b5e0ae3316dafbef",
+    "url": "/search-list/static/media/search-icon.231de5f3.svg"
+  }
+]);
